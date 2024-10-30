730765e05d756d4390acd4d1abe962c335ea9454
Efestus31 <biagiohd@gmail.com>
2024-10-30 21:20:58 +0100

    add comments add configurazione degli asset statici

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,3 +1,5 @@
+
+//creazione rotta per i post
 exports.index = (req, res) => {
     const posts = require('../data/posts.json');
     res.json({ posts: posts, count: posts.length});
--- app/server.js
+++ app/server.js
@@ -1,5 +1,17 @@
+//import express
+const express = require('express')
 const app = express()
+const port = 3000
 
+//creazione rotta per i post nel server locale
+const postsController = require('./controllers/posts.js');
+app.get('/posts', postsController.index);
+
+//base con rotta / che rimanda l'h1
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog!</h1>');
   });
+
+//configurazione degli asset statici 
+app.use(express.static('public'));
+
